4dee02b53b20cf0f2cbfcc19e5e6f9705a3b6005
JersonSS <JersonSS_2000@outlook.com>
2025-04-29 21:08:29 -0500

    nuevo proyecto Api Rest con db

---
+++ AppRestDatabase/src/main/java/pe/senati/AppRestDatabaseApplication.java
@@ -0,0 +1,13 @@
+package pe.senati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRestDatabaseApplication.class, args);
+	}
+
+}

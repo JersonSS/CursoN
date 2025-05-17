f712191f255ad792fbd2d63c9e7cf9a9d8390d3b
JersonSS <JersonSS_2000@outlook.com>
2025-05-17 03:45:07 -0500

    arreglo de consulta de inner join de libro con genero

--- applibadvanced/src/main/java/pe/senati/repository/LibroRepository.java
+++ applibadvanced/src/main/java/pe/senati/repository/LibroRepository.java
@@ -11,9 +11,9 @@ import pe.senati.entity.Libro;
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Integer>
 {
-     @Query(value = "select l.libro_id, l.titulo AS libro, g.genero_id, g.nombre AS genero\n" + //
-                "from libro_genero lg\n" + //
-                "inner join libros l on l.libros_id = lg.genero_id\n" + //
+     @Query(value = "select l.libro_id, l.titulo AS libro, g.genero_id, g.nombre AS genero \n" + //
+                "from libro_genero lg \n" + //
+                "inner join libros l on l.libro_id = lg.libro_id \n" + //
                 "inner join generos g on g.genero_id = lg.genero_id;", nativeQuery = true)
     public abstract Collection<Object[]> findAll_libro_genero();
 
